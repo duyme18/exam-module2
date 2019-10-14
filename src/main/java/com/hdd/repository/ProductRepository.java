cd4f9103db252cf73ad469ad9d1fc559cc3449a5
duy <duyme18@gmail.com>
2019-10-14 17:32:36 +0700

    commit again

--- src/main/java/com/hdd/repository/ProductRepository.java
+++ src/main/java/com/hdd/repository/ProductRepository.java
@@ -9,5 +9,4 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
     Iterable<Product> findAllByCategory(Category category);
 
-    Page<Product> findAllByNameContaining(String name, Pageable pageable);
 }
